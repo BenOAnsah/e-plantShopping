1db0d3b4f5d58d155b32edba9aabc40efe221642
BenOAnsah <benoansah99@gmail.com>
2025-06-19 09:42:24 -0400

    CartItems at the start

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,7 +9,12 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach(item => {
+        const quantity = item.quantity;
+        const cost = parseFloat(item.cost.substring(1));
+        return total += quantity*cost;
+    });
   };
 
   const handleContinueShopping = (e) => {
