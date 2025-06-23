85584ec57f8cde20071f4b5826dfdbc3375e7da6
BenOAnsah <benoansah99@gmail.com>
2025-06-23 17:48:23 +0000

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -32,4 +32,7 @@ export const CartSlice = createSlice({
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
-export default CartSlice.reducer;
+export default cartReducer = CartSlice.reducer;
+
+
+
